91b9f7186f70232914742223b5daa1a5415a706a
srerr22 <cocomryu@gmail.com>
2026-08-29 16:20:59 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,91 +1,101 @@
 /* =========================
-   SPOT DATA
+Re’SURF DATA
+========================= */
+
+/* =========================
+SPOT DATA
 ========================= */
 
 const spots = {
 
-  "辻堂": {
-    latitude: 35.318333,
-    longitude: 139.453056,
+“辻堂”: {
+latitude: 35.318333,
+longitude: 139.453056,
 
-    beachDirection: 202,
+beachDirection: 202,
+// 北〜北東をオフショア基準
+offshoreFrom: 22.5,
+offshoreRange: 30
 
-    offshoreFrom: 22.5,
-    offshoreRange: 30
-  },
+},
 
-  "鵠沼": {
-    latitude: 35.314861,
-    longitude: 139.470889,
+“鵠沼”: {
+latitude: 35.314861,
+longitude: 139.470889,
 
-    beachDirection: 195,
+beachDirection: 195,
+// 北〜北東をオフショア基準
+offshoreFrom: 22.5,
+offshoreRange: 30
 
-    offshoreFrom: 22.5,
-    offshoreRange: 30
-  },
+},
 
-  "由比ヶ浜": {
-    latitude: 35.309444,
-    longitude: 139.537778,
+“由比ヶ浜”: {
+latitude: 35.309444,
+longitude: 139.537778,
 
-    beachDirection: 157,
+beachDirection: 157,
+// 北〜北東をオフショア基準
+offshoreFrom: 22.5,
+offshoreRange: 30
 
-    offshoreFrom: 22.5,
-    offshoreRange: 30
-  },
+},
 
-  "鴨川": {
-    latitude: 35.120797,
-    longitude: 140.132091,
+“鴨川”: {
+// 鴨川＝マルキ
+latitude: 35.120797,
+longitude: 140.132091,
 
-    beachDirection: 157,
+beachDirection: 157,
+// 北西〜北寄りをオフショア基準
+offshoreFrom: 337.5,
+offshoreRange: 30
 
-    offshoreFrom: 337.5,
-    offshoreRange: 30
-  },
+},
 
-  "平砂浦": {
-    latitude: 34.943784,
-    longitude: 139.810939,
+“平砂浦”: {
+// 平砂浦ファミリーパーク下付近
+latitude: 34.943784,
+longitude: 139.810939,
 
-    beachDirection: 202,
+beachDirection: 202,
+// 北東をオフショア基準
+offshoreFrom: 45,
+offshoreRange: 30
 
-    offshoreFrom: 45,
-    offshoreRange: 30
-  }
+}
 
 };
 
-
 /* =========================
-   BOARD TYPES
+BOARD TYPES
 ========================= */
 
 const boards = [
 
-  {
-    id: "long",
-    name: "ロング"
-  },
+{
+id: “long”,
+name: “ロング”
+},
 
-  {
-    id: "mid",
-    name: "ミッド"
-  },
+{
+id: “mid”,
+name: “ミッド”
+},
 
-  {
-    id: "fish",
-    name: "フィッシュ"
-  },
+{
+id: “fish”,
+name: “フィッシュ”
+},
 
-  {
-    id: "allround",
-    name: "オールラウンド"
-  },
+{
+id: “allround”,
+name: “オールラウンド”
+},
 
-  {
-    id: "performance",
-    name: "パフォーマンス"
-  }
+{
+id: “performance”,
+name: “パフォーマンス”
+}
 
 ];
