2e202729ab5b9ffeeb8fbb816ac77d3fc57371a4
srerr22 <cocomryu@gmail.com>
2026-08-29 15:16:22 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,91 @@
+/* =========================
+   SPOT DATA
+========================= */
+
+const spots = {
+
+  "辻堂": {
+    latitude: 35.318333,
+    longitude: 139.453056,
+
+    beachDirection: 202,
+
+    offshoreFrom: 22.5,
+    offshoreRange: 30
+  },
+
+  "鵠沼": {
+    latitude: 35.314861,
+    longitude: 139.470889,
+
+    beachDirection: 195,
+
+    offshoreFrom: 22.5,
+    offshoreRange: 30
+  },
+
+  "由比ヶ浜": {
+    latitude: 35.309444,
+    longitude: 139.537778,
+
+    beachDirection: 157,
+
+    offshoreFrom: 22.5,
+    offshoreRange: 30
+  },
+
+  "鴨川": {
+    latitude: 35.120797,
+    longitude: 140.132091,
+
+    beachDirection: 157,
+
+    offshoreFrom: 337.5,
+    offshoreRange: 30
+  },
+
+  "平砂浦": {
+    latitude: 34.943784,
+    longitude: 139.810939,
+
+    beachDirection: 202,
+
+    offshoreFrom: 45,
+    offshoreRange: 30
+  }
+
+};
+
+
+/* =========================
+   BOARD TYPES
+========================= */
+
+const boards = [
+
+  {
+    id: "long",
+    name: "ロング"
+  },
+
+  {
+    id: "mid",
+    name: "ミッド"
+  },
+
+  {
+    id: "fish",
+    name: "フィッシュ"
+  },
+
+  {
+    id: "allround",
+    name: "オールラウンド"
+  },
+
+  {
+    id: "performance",
+    name: "パフォーマンス"
+  }
+
+];
